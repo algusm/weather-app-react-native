d22f26fe02b3107d7e6a7041fa88577d8e7159d6
Alex Montana <algusm@gmail.com>
2024-02-01 21:44:38 -0300

    Improving user interface

--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -1,23 +1,53 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { Props } from "../types/navigation";
-import { TextInput, Text, Surface, IconButton } from "react-native-paper";
+import { Divider, Text, Surface, Icon } from "react-native-paper";
 
 export default function DailyForecast({route, navigation} : Props) {
 
     const {dailyForecast} =route.params
 
+    const icons = new Map([
+        ["Thunderstorm", "weather-lightning"],
+        ["Drizzle", "weather-pouring"],
+        ["Rain", "weather-pouring"],
+        ["Snow", "snowflake"],
+        ["Clear", "white-balance-sunny"],
+        ["Clouds", "weather-cloudy"],
+        ["Mist", "weather-dust"],
+        ["Smoke", "weather-dust"],
+        ["Haze", "weather-dust"],
+        ["Dust", "weather-dust"],
+        ["Fog", "weather-dust"],
+        ["Sand", "weather-dust"],
+        ["Dust", "weather-dust"],
+        ["Ash", "weather-dust"],
+        ["Squall", "weather-dust"],
+        ["Tornado", "weather-dust"]
+    ]);
+
     return (
-        <View>
+        <ScrollView>
             {dailyForecast.map((hourlyForecast) =>(
             
-            <Surface elevation={2}>
-                <Text>
-                    {hourlyForecast.date.toLocaleTimeString()} {hourlyForecast.weatherType} {hourlyForecast.temperature}
-                </Text>
+            <Surface elevation={2} style={{padding:10, margin: 5, borderRadius : 10}}>
+                <View style={{flexDirection : "row"}}>
+                    <Text style={{flex : 9}}>lorem ipsum</Text>
+                    <Icon source={icons.get(hourlyForecast.weatherType)} size={40}/>
+                </View>
+                <View style={{marginBottom : 50, marginTop : 50}}>
+                    <Text style={{textAlign : "center"}} variant="displayLarge">{hourlyForecast.temperature}</Text>
+                    <Text style={{textAlign : "center"}} variant="bodySmall">{hourlyForecast.weatherDescription}</Text>
+                    <Text style={{textAlign : "center"}} variant="bodySmall">{hourlyForecast.date.toLocaleString()}</Text>
+                </View>
+                <Divider/>
+                <View>
+                <Text>lorem ipsum</Text>
+                </View>
+                
             </Surface>
             
           ))}
-        </View>
+        </ScrollView>
     );
 
 }
--- src/components/WeeklyForecast.tsx
+++ src/components/WeeklyForecast.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, View, Pressable } from "react-native";
 import { TextInput, Text, Surface, IconButton } from "react-native-paper";
 import findForecastByCity from "../services/weather";
 import DailyWeatherForecast from "../types/dailyWeatherForecast";
@@ -16,24 +16,34 @@ export default function WeeklyForecast({navigation}: WeeklyProps) {
     } 
     
     return (
-        <SafeAreaView>
-          <TextInput mode="outlined" label="Cidade" onChangeText={setCityName} value={cityName} right={<TextInput.Icon icon="magnify" onPress={searchLocation}/>}/>
+        <View>
+          <Text variant="titleMedium" style={{marginTop : 20, paddingLeft : 10}}>Selecione uma cidade</Text>
+          <TextInput mode="outlined" label="Cidade" onChangeText={setCityName} value={cityName} right={<TextInput.Icon icon="magnify" onPress={searchLocation}/>} style={{marginBottom : 20}}/>
+          <Text style={{paddingLeft : 10}}>Previsão para a semana:</Text>
           {weeklyForecast.map((dailyForecast) =>(
             
-            <Surface elevation={2}>
-                <Text>
+            <Pressable onPress={() => {
+                navigation.navigate('Daily', {dailyForecast : dailyForecast.hourlyForecastList})
+            }}>
+            <Surface elevation={2} style={{padding:10, margin: 5, flexDirection : "row", borderRadius : 10}}>
+                <Text variant="titleLarge" style={{flex :4}}>
                     {getDayOfWeek(dailyForecast.date)}
                 </Text>
-                <Text>
-                    min. {dailyForecast.minimumTemperature} max {dailyForecast.maximumTemperature}
-                </Text>
-                <IconButton icon="magnify" onPress={() => {
-                    navigation.navigate('Daily', {dailyForecast : dailyForecast.hourlyForecastList})
-                }}/>
+                <View style={{flex :2, flexDirection : "row"}}>
+                    <Text style={{paddingTop : 7}}>min.</Text>
+                    <Text variant="titleLarge">
+                        {dailyForecast.minimumTemperature}
+                    </Text>
+                </View>
+                <View style={{flex :2, flexDirection : "row"}}>
+                    <Text style={{paddingTop : 7}}>max.</Text>
+                    <Text variant="titleLarge">{dailyForecast.maximumTemperature}</Text>
+                </View>
             </Surface>
+            </Pressable>
             
           ))}
-        </SafeAreaView>
+        </View>
     );
 }
 
